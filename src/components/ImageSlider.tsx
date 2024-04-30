7f8e238ca899cec116088eef06d1f0796182b12f
Guilherme Illescas <gui.illescas@icloud.com>
2024-04-30 12:17:42 -0300

    feat: implemented slider on home product cards

---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,100 @@
+import { ReactElement, useEffect, useState } from 'react'
+import Image from 'next/image'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper/modules'
+import type SwiperType from 'swiper'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+
+interface ImageSliderProps {
+  urls: string[]
+}
+
+export default function ImageSlider({ urls }: ImageSliderProps): ReactElement {
+  const [swiper, setSwiper] = useState<null | SwiperType>(null)
+  const [activeIndex, setActiveIndex] = useState(0)
+  const [slideConfig, setSlideConfig] = useState({
+    isBeginning: true,
+    isEnd: activeIndex === (urls.length ?? 0) - 1,
+  })
+
+  useEffect(() => {
+    swiper?.on('slideChange', ({ activeIndex }) => {
+      setActiveIndex(activeIndex)
+      setSlideConfig({
+        isBeginning: activeIndex === 0,
+        isEnd: activeIndex === (urls.length ?? 0) - 1,
+      })
+    })
+  }, [swiper, urls])
+
+  const activeStyles =
+    'active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 items-center justify-center rounded-full border-2 bg-white border-zinc-300'
+  const inactiveStyles = 'hidden text-gray-400'
+
+  return (
+    <div className='group relative aspect-square overflow-hidden rounded-xl bg-zinc-100'>
+      <div className='absolute inset-0 opacity-0 transition zoom-in-100 group-hover:opacity-100'>
+        <button
+          onClick={(event) => {
+            event.preventDefault()
+            swiper?.slideNext()
+          }}
+          className={cn(activeStyles, 'right-3 transition', {
+            [inactiveStyles]: slideConfig.isEnd,
+            'hover:bg-primary-300 text-primary-800 opacity-100':
+              !slideConfig.isEnd,
+          })}
+          aria-label='Next image'
+        >
+          <ChevronRight className='h-4 w-4 text-zinc-700' />
+        </button>
+
+        <button
+          onClick={(event) => {
+            event.preventDefault()
+            swiper?.slidePrev()
+          }}
+          className={cn(activeStyles, 'left-3 transition', {
+            [inactiveStyles]: slideConfig.isBeginning,
+            'hover:bg-primary-300 text-primary-800 opacity-100':
+              !slideConfig.isBeginning,
+          })}
+          aria-label='Previous image'
+        >
+          <ChevronLeft className='h-4 w-4 text-zinc-700' />
+        </button>
+      </div>
+
+      <Swiper
+        pagination={{
+          renderBullet: (_, className) => {
+            return `<span class="rounded-full transition ${className}"></span>`
+          },
+        }}
+        onSwiper={(swiper) => setSwiper(swiper)}
+        spaceBetween={50}
+        modules={[Pagination]}
+        slidesPerView={1}
+        className='h-full w-full'
+      >
+        {urls.map((url, index) => (
+          <SwiperSlide key={index} className='relative -z-10 h-full w-full'>
+            <Image
+              fill
+              loading='eager'
+              className='-z-10 h-full w-full object-cover object-center'
+              src={url}
+              alt='product image'
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  )
+}
---
+++ src/components/ProductReel.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import { ReactElement } from 'react'
+import Link from 'next/link'
+
+import { trpc } from '@/trpc/client'
+
+import { TQueryValidator } from '@/lib/validators/query'
+
+import { Product } from '@/payload-types'
+
+import ProductListing from './ProductListing'
+
+interface ProductReelProps {
+  title: string
+  subTitle?: string
+  href?: string
+  query: TQueryValidator
+}
+
+const FALLBACK_LIMIT = 4
+
+export default function ProductReel(props: ProductReelProps): ReactElement {
+  const { title, subTitle, href, query } = props
+
+  const { data: queryResults, isLoading } =
+    trpc.getInfiniteProducts.useInfiniteQuery(
+      {
+        limit: query.limit ?? FALLBACK_LIMIT,
+        query,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage.nextPage,
+      }
+    )
+
+  const products = queryResults?.pages.flatMap((page) => page.items)
+
+  let map: (Product | null)[] = []
+  if (products && products.length) {
+    map = products
+  } else if (isLoading) {
+    map = new Array<null>(query.limit ?? FALLBACK_LIMIT).fill(null)
+  }
+
+  return (
+    <section className='py-12'>
+      <div className='mb-4 md:flex md:items-center md:justify-between'>
+        <div className='max-w-2xl px-4 lg:max-w-4xl lg:px-0'>
+          {title ? (
+            <h1 className='to-gray-900 text-2xl font-bold sm:text-3xl'>
+              {title}
+            </h1>
+          ) : null}
+          {subTitle ? (
+            <p className='mt-2 text-sm text-muted-foreground'>{subTitle}</p>
+          ) : null}
+        </div>
+
+        {href ? (
+          <Link
+            href={href}
+            className='hidden text-sm font-medium text-blue-600 hover:text-blue-500 md:block'
+          >
+            Shop the collection <span aria-hidden='true'>&rarr;</span>
+          </Link>
+        ) : null}
+      </div>
+
+      <div className='relative'>
+        <div className='mt-6 w-full items-center'>
+          <div className='grid w-full grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8'>
+            {map.map((product, index) => (
+              <ProductListing product={product} index={index} key={index} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
