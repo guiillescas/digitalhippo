76ef8fad2baa66124ff1c491f02dcc918c051076
Guilherme Illescas <gui.illescas@icloud.com>
2025-01-13 08:45:14 -0300

    feat: created use cart hook

---
+++ src/hooks/use-cart.ts
@@ -0,0 +1,34 @@
+import { createJSONStorage, persist } from 'zustand/middleware'
+import { create } from 'zustand'
+
+import { Product } from '@/payload-types'
+
+export type CartItem = {
+  product: Product
+}
+
+type CartState = {
+  items: CartItem[]
+  addItem: (product: Product) => void
+  removeItem: (productId: string) => void
+  clearCart: () => void
+}
+
+export const useCart = create<CartState>()(
+  persist(
+    (set) => ({
+      items: [],
+      addItem: (product) =>
+        set((state) => ({ items: [...state.items, { product }] })),
+      removeItem: (productId) =>
+        set((state) => ({
+          items: state.items.filter((item) => item.product.id !== productId),
+        })),
+      clearCart: () => set({ items: [] }),
+    }),
+    {
+      name: '@digital-hippo:cart-storage',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+)
