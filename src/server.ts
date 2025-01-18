6c1f79bfef431142152b4e03ab1ea4817b0504bd
Guilherme Illescas <gui.illescas@icloud.com>
2025-01-18 19:08:59 -0300

    feat: configured server build

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+import nextBuild from 'next/dist/build'
+
+import path from 'path'
 import { IncomingMessage } from 'http'
 import express from 'express'
+import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { inferAsyncReturnType } from '@trpc/server'
@@ -10,6 +14,8 @@ import { nextApp, nextHandler } from './next-utils'
 
 import { appRouter } from './trpc'
 import { getPayloadClient } from './get-payload'
+
+dotenv.config()
 
 const app = express()
 const port = Number(process.env.PORT) || 3000
@@ -30,8 +36,6 @@ const start = async () => {
     },
   })
 
-  app.post('/api/webhooks/stripe', webhookMiddleware, stripeWebhookHandler)
-
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
@@ -40,6 +44,21 @@ const start = async () => {
       },
     },
   })
+
+  app.post('/api/webhooks/stripe', webhookMiddleware, stripeWebhookHandler)
+
+  if (process.env.NEXT_BUILD) {
+    app.listen(port, async () => {
+      payload.logger.info(`🚀 Server is building for production`)
+
+      // @ts-expect-error missing types
+      await nextBuild(path.join(__dirname, '../'))
+
+      process.exit()
+    })
+
+    return
+  }
 
   app.use(
     '/api/trpc',
