6c219716e11ef9db96da24e61ac177102533333b
Guilherme Illescas <gui.illescas@icloud.com>
2025-01-22 23:48:58 -0300

    feat: added env validation to stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,12 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
+const secretKey = process.env.STRIPE_SECRET_KEY
+
+if (!secretKey) {
+  throw new Error('STRIPE_SECRET_KEY is missing')
+}
+
+export const stripe = new Stripe(secretKey, {
   apiVersion: '2024-12-18.acacia',
   typescript: true,
 })
